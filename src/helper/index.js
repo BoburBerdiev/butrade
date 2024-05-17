12a15890fb95f721e04aa0fe72ce21c2e32b666e
BoburBerdiev <sirofim012@gmail.com>
2024-05-17 17:40:49 +0500

    pull mergin

---
+++ src/helper/index.js
@@ -0,0 +1,28 @@
+
+export const priceView = (price) => {
+   return  price?.toLocaleString('en-US', { style: 'decimal' }).replace(/,/g, ' ')
+}
+
+export const formatPhoneNumber =(originalString) => {
+   return originalString?.replace(/(\d{3})(\d{2})(\d{3})(\d{2})(\d{2})/, '$1 $2 $3 $4 $5');
+}
+
+
+export const PageWayRouter  = ( pageAsPath , findPage) => {
+   const pageSplit = pageAsPath.split('/')
+
+   if(findPage) {
+     return  pageSplit?.find(item => item === findPage)
+   }
+
+   
+}
+
+
+export const langSelect = ( lang , ru  ,uz) => {
+   if(lang === 'ru')  {
+       return ru
+   }else if(lang === 'uz') {
+       return uz
+   }
+}
